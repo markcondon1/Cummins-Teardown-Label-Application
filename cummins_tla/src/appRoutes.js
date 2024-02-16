5e75b4916b85bb45d7752eeea4951cd2d5f2b947
LexiPadberg <89533377+LexiPadberg@users.noreply.github.com>
2024-02-15 21:46:22 -0500

    set up navigation page

---
+++ cummins_tla/src/appRoutes.js
@@ -0,0 +1,23 @@
+import App from "./App";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import FirstFit from "./pages/FirstFit";
+import Login from "./pages/Login";
+import Reman from "./pages/Reman";
+import TeardownTray from "./pages/TeardownTray";
+export default function appRoutes(){
+
+
+    const navigate = useNavigate();
+    return(
+        <Routes>
+            <Route exact path="/" element={<Login/>}/>
+            <Route exact path="/app" element={<App/>}/>
+            <Route extact path="/app/home" element={<Dasboard/>}/>
+            <Route exact path="/app/firstFit" element={<FirstFit/>}/>
+            <Route exact path="/app/Reman" element={<Reman/>}/>
+            <Route exact path="/app/teardownTray" element={<TeardownTray/>}/>
+        </Routes>
+    )
+
+}
