bdc746a57d722b660c89ea536b297a9aa4f3a68a
LexiPadberg <89533377+LexiPadberg@users.noreply.github.com>
2024-02-20 16:52:27 -0500

    login fixed

--- cummins_tla/src/pages/login.js
+++ cummins_tla/src/pages/login.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Image from 'react-bootstrap/Image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.png';
-import {useNavigate} from "react-router-dom";
+
 
 export default function Login(){
     const navigate = useNavigate();
@@ -15,6 +15,7 @@ export default function Login(){
             <div className="col-12 col-lg-6">
                 <h1><img src={logo} width={75} height={75}/>Cummins Unified Teardown Label
                     Application <Button>Logout</Button>{' '}</h1>
+                <Button onClick={login}> login</Button>
 
             </div>
         </div>
