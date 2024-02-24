d95a31fa8749bb867bdecdebd6347f9261a346d9
LexiPadberg <89533377+LexiPadberg@users.noreply.github.com>
2024-02-24 09:59:08 -0500

    login functionality now working with database connection

--- cummins_tla/src/pages/components/NavBar.js
+++ cummins_tla/src/pages/components/NavBar.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { slide as Menu } from 'react-burger-menu'
+import { slide as Menu } from 'react-burger-menu';
 import logo from './logo.png';
 import './NavBar.css';
 
