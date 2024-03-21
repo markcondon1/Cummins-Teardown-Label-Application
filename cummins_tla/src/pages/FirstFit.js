f9e6e5b2a263cced26d9f17bac1bbaa64fb542c6
markcondon1 <132102190+markcondon1@users.noreply.github.com>
2024-03-21 19:44:14 -0400

    Creation of First Fit ZPL and print

--- cummins_tla/src/pages/FirstFit.js
+++ cummins_tla/src/pages/FirstFit.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import NavBar from "./components/NavBar";
 import  './page_styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import jsPDF from "jspdf";
 import {useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import axios from "axios";
@@ -14,17 +15,153 @@ export default function FirstFit(){
 
     console.log("user: ", user);
 
-    const reman=()=>{
-        navigate("/app/Reman");
-    }
-    const teardown = () => {
-        navigate("/app/teardownTray");
+    var compressorBool = false;
+    var turbineBool = false;
+
+    console.log(compressorBool);
+
+    const printLabel = () => {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear(); // Get the current year
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const hours = String(currentDate.getHours()).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+        let start = new Date(currentDate.getFullYear(), 0, 0);
+        let diff = (currentDate - start) + ((start.getTimezoneOffset() - currentDate.getTimezoneOffset()) * 60 * 1000);
+        let oneDay = 1000 * 60 * 60 * 24;
+        let dayOfYear = Math.floor(diff / oneDay).toString().padStart(3,'0');
+        
+        const date =`${month}/${day}/${year}`;
+        const time = `${hours}:${minutes}:${seconds}`;
+
+        const twoDigitYear = year.toString().slice(-2);
+
+        const serial = handleSerial().toString().padStart(4,'0');
+
+        console.log("compressor "+compressorBool);
+        if(turbineBool = true)
+        {
+            const itemsegment = ``;
+
+        const matrixContent = `P${itemsegment}S${twoDigitYear}${dayOfYear}${serial}V0TDRC`;
+            const zpl = `
+            ^XA
+            ^FX Date/Time
+            ^FO5,25^A 0,30,30^FD ${time}^FS
+            ^FO125,25^A 0,30,30^FD ${date}^FS
+                
+            ^FX User ID
+            ^FO310,25^A 0,30,30^FD${user.userid}^FS
+        
+            ^FX Turbine/Compressor Housing
+            ^FO5,55^A 0,30,30^FD Turbine Housing:^FS
+            ^FO310,55^A 0,30,30^FD3592787^FS
+        
+            ^FX TD SQ
+            ^FO5,85^A 0,30,30^FD TD SEQ:^FS
+            ^FO310,85^A 0,30,30^FD2402070053 ^FS
+        
+            ^FO180,145^BXN,5,200,20,20,3,,1
+                ^FD${matrixContent}^FS
+        
+            ^XZ 
+            `;
+            // Create a new instance of jsPDF
+            const doc = new jsPDF();
+            // Add ZPL content to PDF
+            doc.text(zpl, 10, 10);
+            // Save PDF
+            doc.save('label.pdf');
+            
+        }
+        if(compressorBool == true)
+        {
+            console.log("inside")
+            
+        
+            const itemsegment = ``;
+
+            const matrixContent = `P${itemsegment}S${twoDigitYear}${dayOfYear}${serial}V0TDRC`;
+            const zpl = `
+            ^XA
+            ^FX Date/Time
+            ^FO5,25^A 0,30,30^FD ${time}^FS
+            ^FO125,25^A 0,30,30^FD ${date}^FS
+        
+            ^FX User ID
+            ^FO310,25^A 0,30,30^FD${user.userid}^FS
+
+            ^FX Turbine/Compressor Housing
+            ^FO5,55^A 0,30,30^FD Compressor Housing:^FS
+            ^FO310,55^A 0,30,30^FD3592787^FS
+
+            ^FX TD SQ
+            ^FO5,85^A 0,30,30^FD TD SEQ:^FS
+            ^FO310,85^A 0,30,30^FD2402070053 ^FS
+
+            ^FO180,145^BXN,5,200,20,20,3,,1
+            ^FD${matrixContent}^FS
+
+            ^XZ 
+            `;
+            // Create a new instance of jsPDF
+            const doc = new jsPDF();
+            // Add ZPL content to PDF
+            doc.text(zpl, 10, 10);
+            // Save PDF
+            doc.save('label.pdf');
+        }
     }
 
-    const firstFit = () =>{
-        navigate("/app/firstFit");
-    }
 
+    const handleSerial = () =>{
+        const currentDate = new Date();
+        if(localStorage.getItem('serialData') !== null){
+            let serialData = JSON.parse(localStorage.getItem('serialData'));
+            const localDate = new Date(serialData.date);
+            if(localDate.getDate() === currentDate.getDate() && localDate.getMonth() === currentDate.getMonth()){
+                serialData.serial += 1;
+                localStorage.setItem('serialData', JSON.stringify(serialData))
+                return JSON.parse(localStorage.getItem('serialData')).serial;
+            } else{
+                localStorage.removeItem('serialData');
+                return createSerial();
+            }
+        } else{
+            return createSerial();
+        }
+    };
+
+    const createSerial = () =>{
+        const currentDate = new Date();
+        const serialData = {
+            serial:1,
+            date:currentDate,
+        };
+        localStorage.setItem('serialData', JSON.stringify(serialData))
+        return JSON.parse(localStorage.getItem('serialData')).serial;
+    };
+
+    function updateBool(radioInput){
+        if(radioInput == 1){
+            console.log("radioinput 1")
+            turbineBool = true;
+            compressorBool = false;
+        }
+        else if(radioInput == 2){
+            console.log("radioinput 2")
+            turbineBool = false;
+            compressorBool = true;
+        }
+        else if(radioInput == 3){
+            console.log("radioinput 3")
+            turbineBool = true;
+            compressorBool = true;
+        }
+    }
 
     return(
         <div className="container-flex">
@@ -54,11 +191,11 @@ export default function FirstFit(){
                                 </div>
                                 <div className="radio-buttons">
                                     <label>Print:</label>
-                                    <input type="radio" name="component" value="turbine" id="turbine" />
+                                    <input type="radio" name="component" value="turbine" id="turbine" onClick={updateBool(1)}/>
                                     <label htmlFor="turbine">Turbine Housing</label>
-                                    <input type="radio" name="component" value="compressor" id="compressor" />
+                                    <input type="radio" name="component" value="compressor" id="compressor" onClick={updateBool(2)}/>
                                     <label htmlFor="compressor">Compressor Housing</label>
-                                    <input type="radio" name="component" value="both" id="both" />
+                                    <input type="radio" name="component" value="both" id="both" onClick={updateBool(3)}/>
                                     <label htmlFor="both">Both</label>
                                 </div>
                                 <div className="print-controls">
@@ -66,7 +203,7 @@ export default function FirstFit(){
                                         <input type="checkbox" id="autoPrint" />
                                         <label htmlFor="autoPrint">Auto Print</label>
                                     </div>
-                                    <button className="print-button">Print</button>
+                                    <button className="print-button" onClick={printLabel}>Print</button>
                                 </div>
                             </div>
                         </div>
